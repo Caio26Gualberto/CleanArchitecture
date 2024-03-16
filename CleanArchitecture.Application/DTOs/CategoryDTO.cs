d8c6dc2b3166d0c2277fe28a1204cc7a2e9fd9b5
Caio26Gualberto <caiogualbertodev@outlook.com>
2024-03-16 15:34:06 -0300

    Criando os DTOs e os mapeamentos com AutoMapper

---
+++ CleanArchitecture.Application/DTOs/CategoryDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanArchitecture.Application.DTOs
+{
+    public class CategoryDTO
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "O nome é necessário")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        public string Name { get; set; }
+    }
+}
