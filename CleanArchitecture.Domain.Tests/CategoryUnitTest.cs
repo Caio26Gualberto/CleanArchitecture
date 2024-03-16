69004b92f9f2254d14a595ba9f1bee400bf7d716
Caio26Gualberto <caiogualbertodev@outlook.com>
2024-03-15 23:51:29 -0300

    Criando testes para a Entidade Product

--- CleanArchitecture.Domain.Tests/CategoryUnitTest.cs
+++ CleanArchitecture.Domain.Tests/CategoryUnitTest.cs
@@ -14,28 +14,28 @@ namespace CleanArchitecture.Domain.Tests
         }
 
         [Fact(DisplayName = "CreateCategoryNegativeId")]
-        public void CreateCategory_NegativeIdValue_DomainExceptionInvalidId()
+        public void CreateCategory_NegativeIdValue_DomainException()
         {
             Action action = () => new Category(-1, "Category Name");
             action.Should().Throw<DomainExceptionValidation>();
         }
 
         [Fact(DisplayName = "CreateCategoryShortName")]
-        public void CreateCategory_ShortNameValue_DomainExceptionShortName()
+        public void CreateCategory_ShortNameValue_DomainException()
         {
             Action action = () => new Category(1, "Ca");
             action.Should().Throw<DomainExceptionValidation>();
         }
 
         [Fact(DisplayName = "CreateCategoryRequiredName")]
-        public void CreateCategory_MissingNameValue_DomainExceptionRequiredName()
+        public void CreateCategory_MissingNameValue_DomainException()
         {
             Action action = () => new Category(1, "");
             action.Should().Throw<DomainExceptionValidation>();
         }
 
         [Fact(DisplayName = "CreateCategoryNullName")]
-        public void CreateCategory_WithNullNameValue_DomainExceptionInvalidName()
+        public void CreateCategory_WithNullNameValue_DomainException()
         {
             Action action = () => new Category(1, null);
             action.Should().Throw<DomainExceptionValidation>();
---
+++ CleanArchitecture.Domain.Tests/ProductUnitTest.cs
@@ -0,0 +1,65 @@
+﻿using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.Validation;
+using FluentAssertions;
+
+namespace CleanArchitecture.Domain.Tests
+{
+    public class ProductUnitTest
+    {
+        [Fact(DisplayName = "CreateProduct")]
+        public void CreateProduct_WithValidParameters_ResultObjectValidState()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, "Product Image");
+            action.Should().NotThrow<DomainExceptionValidation>();
+        }
+
+        [Fact(DisplayName = "CreateProductNegativeId")]
+        public void CreateProduct_NegativeIdValue_DomainException()
+        {
+            Action action = () => new Product(-1, "Product Name", "Product Description", 9.99m, 99, "Product Image");
+            action.Should().Throw<DomainExceptionValidation>();
+        }
+
+        [Fact(DisplayName = "CreateProductShortName")]
+        public void CreateProduct_ShortNameValue_DomainException()
+        {
+            Action action = () => new Product(1, "Pr", "Product Description", 9.99m, 99, "Product Image");
+            action.Should().Throw<DomainExceptionValidation>();
+        }
+
+        [Fact(DisplayName = "CreateProductLongImageName")]
+        public void CreateProduct_LongImageName_DomainException()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, "Imageeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
+            action.Should().Throw<DomainExceptionValidation>();
+        }
+
+        [Fact(DisplayName = "CreateProductNullImageName")]
+        public void CreateProduct_NullImageName_DomainException()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, null);
+            action.Should().NotThrow<DomainExceptionValidation>();
+        }
+
+        [Fact(DisplayName = "CreateProductEmptyImageName")]
+        public void CreateProduct_EmptyImageName_DomainException()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, "");
+            action.Should().NotThrow<DomainExceptionValidation>();
+        }
+
+        [Fact(DisplayName = "CreateProductInvalidStockValue")]
+        public void CreateProduct_InvalidStockValue_DomainException()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, -5, "");
+            action.Should().Throw<DomainExceptionValidation>();
+        }
+
+        [Fact(DisplayName = "CreateProductInvalidPriceValue")]
+        public void CreateProduct_InvalidPriceValue_DomainException()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", -9.99m, 5, "");
+            action.Should().Throw<DomainExceptionValidation>();
+        }
+    }
+}
