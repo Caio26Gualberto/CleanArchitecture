2772de08856ef2749101c076ebe5576cb0bef5f5
Caio26Gualberto <caiogualbertodev@outlook.com>
2024-03-18 20:31:08 -0300

    Implementando Identity e customizando as tabelas do mesmo para o meu gosto pessoal, criando interfaces para autenticação e seeds de roles e claims do usuário e implementação da service de autenticação do usuário

---
+++ CleanArchitecture.Infra.Data/Identity/AuthenticateService.cs
@@ -0,0 +1,44 @@
+﻿using CleanArchitecture.Domain.Account;
+using Microsoft.AspNetCore.Identity;
+
+namespace CleanArchitecture.Infra.Data.Identity
+{
+    public class AuthenticateService : IAuthenticate
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AuthenticateService(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public async Task<bool> Authenticate(string email, string password)
+        {
+            var result = await _signInManager.PasswordSignInAsync(email, password, false, lockoutOnFailure: false);
+            return result.Succeeded;
+        } 
+
+        public async Task<bool> RegisterUser(string email, string password)
+        {
+            User user = new User
+            {
+                UserName = email,
+                Email = email
+            };
+
+            var result = await _userManager.CreateAsync(user, password);
+                
+            if (result.Succeeded)
+                await _signInManager.SignInAsync(user, isPersistent: false);
+
+            return result.Succeeded;    
+        }
+
+        public async Task Logout()
+        {
+            await _signInManager.SignOutAsync();
+        }
+    }
+}
