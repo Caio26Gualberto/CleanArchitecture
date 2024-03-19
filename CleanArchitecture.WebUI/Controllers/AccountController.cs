ea2396d1ff1c68bd8726c81f2a178ee339b15619
Caio26Gualberto <caiogualbertodev@outlook.com>
2024-03-18 23:41:39 -0300

    Criando o controlador AccountController e as ViewModels na camada WebUI

---
+++ CleanArchitecture.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using CleanArchitecture.Domain.Account;
+using CleanArchitecture.WebUI.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IAuthenticate _authentication;
+        public AccountController(IAuthenticate authenticate)
+        {
+            _authentication = authenticate;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            return View(model);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            return View();
+        }
+    }
+}
---
+++ CleanArchitecture.WebUI/Model/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanArchitecture.WebUI.Model
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Email é necessário")]
+        [EmailAddress(ErrorMessage = "Formato inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Uma senha é necessária")]
+        [StringLength(20, ErrorMessage = "A senha precisa ter no máximo 20 caracteres e no minimo 6", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
